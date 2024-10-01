48aacc133fa8d1180a4c32a71926fd450b725947
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-10-01 13:14:21 +0300

    third_commit

--- db/db.py
+++ db/db.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from env import settings
 
-DB_HOST = settings.DB_HOST
-DB_NAME = settings.DB_NAME
-DB_USER = settings.DB_USER
-DB_PASSWORD = settings.DB_USER
+# DB_HOST = settings.DB_HOST
+# DB_NAME = settings.DB_NAME
+# DB_USER = settings.DB_USER
+# DB_PASSWORD = settings.DB_USER
+DB_HOST:str = 'localhost'
+DB_NAME:str = 'book-store'
+DB_USER:str = 'postgres'
+DB_PASSWORD:int = "7504"
 
 engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
 
--- router/auth_router.py
+++ router/auth_router.py
@@ -4,6 +4,9 @@ from db.db import Session
 import bcrypt,secrets
 authRouter=APIRouter()
 registered_user_session={}
+class UserSession:
+    def __init__(self, user):
+        self.user = user
 @authRouter.post("/register")
 async def register_func(user:UserModel,response:Response,request:Request):
     try:
@@ -19,26 +22,27 @@ async def register_func(user:UserModel,response:Response,request:Request):
         print(e)
         return {"message":"error registering user"}
 @authRouter.post("/login")
-async def login_func(user:UserModel,response:Response,request:Request):
-    session=Session()
+@authRouter.post("/login")
+async def login_func(user: UserModel, response: Response, request: Request):
+    session = Session()
     try:
-        user_db=session.query(User).filter_by(email=user.email).first()
-        if user_db!=None:
-            if user_db.name!=user.name:
-                return {"message":"invalid username or password"}
+        user_db = session.query(User).filter_by(email=user.email).first()
+        if user_db is not None:
+            if user_db.name != user.name:
+                return {"message": "invalid username or password"}
             hashed_password = user_db.password.encode("utf-8")
             if bcrypt.checkpw(user.password.encode("utf-8"), hashed_password):
-               session_id = secrets.token_urlsafe(16)
-               registered_user_session[session_id] = Session(user=user)
-               response.set_cookie("session_id", session_id)
-               return {"message":"successful login"}
+                session_id = secrets.token_urlsafe(16)
+                registered_user_session[session_id] = UserSession(user)
+                response.set_cookie("session_id", session_id)
+                return {"message": "successful login"}
             else:
-               return {"message":"invalid username or password"}
+                return {"message": "invalid username or password"}
         else:
-            return {"message":"user not found"}
+            return {"message": "user not found"}
     except Exception as e:
         print(e)
-        return {"message":"error login in"}
+        return {"message": "error logging in"}
         
 
 @authRouter.post("/logout")
